6a5f527f35e820b4916aeb2539f2c94d7c8df616
mjrock <mj11rock@gmail.com>
2020-02-11 20:26:58 +0500

    Giphy API connect

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/card.vue";
+
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Card
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/dist/css/bootstrap.css";
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <!-- <div class="col-4 bg-danger m-2" v-for="todo of todos" :key="todo.id">{{todo.id}}</div> -->
+      <img :src="gifs.images.downsized_large.url" alt />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Card",
+  data() {
+    return {
+      todos: [],
+      gifs: [],
+      giphyURL:
+        "https://api.giphy.com/v1/gifs/random?api_key=CbSHaXroSuxGYrTZ95AdA5XHGptvSb6u&tag=doughnut&rating=pg-13"
+    };
+  },
+
+  async created() {
+    // Giphy API URL
+    const res = await axios.get(this.giphyURL);
+
+    this.gifs = res.data.data;
+  },
+  methods: {
+    newGif() {
+      axios.get(this.giphyURL).then(resp => {
+        this.gifs = resp.data;
+      });
+    },
+    renderGif() {}
+  }
+};
+</script>
+
+<style scoped></style>
